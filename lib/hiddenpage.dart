e581816b1f4f39aa67807dcf49bd1ee704fb2f44
surakshns <surakshnss@gmail.com>
2020-10-21 13:20:09 +0530

    added hidden page error correction

--- lib/hiddenpage.dart
+++ lib/hiddenpage.dart
@@ -6,31 +6,35 @@ import 'ReuseableCard.dart';
 class hiddenpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-          ReuseableCode(
-          colour: kactivecardcolor,
-             cardicons: Column(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: <Widget>[
-                 Text('This is Ideal BMI',
-                   style: fNormalfont,
+    return Scaffold(
+      body: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 4,
+                  child: ReuseableCode(
+                    colour: kactivecardcolor,
+                    cardicons: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                     Text('This is Ideal BMI',
+                       style: fNormalfont,
+                     ),
+                     SizedBox(
+                       height: 50,
+                     ),
+                     Text('Yay!You have unlocked the hidden feature.',
+                     style: fOutputString,textAlign: TextAlign.center,
+                     )
+                   ],
                  ),
-                 SizedBox(
-                   height: 50,
                  ),
-                 Text('Yay!You have unlocked the hidden feature',
-                 style: fOutputString,textAlign: TextAlign.center,
-                 )
-               ],
-             ),
-             )
-            ],
+            )
+              ],
+            ),
           ),
-        ),
     );
   }
 }
